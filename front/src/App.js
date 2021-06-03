89b3bc8b825aaa59caa9202704a979896879f73b
GoodPart <pks940122@cttd.co.kr>
2021-06-03 20:59:53 +0900

    셋팅중

---
+++ front/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import Home from "./page/Home";
+import About from "./page/About";
+
+import dotenv from "dotenv";
+
+import "./assets/scss/common.scss";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="header">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <h1>test</h1>
+      {process.env.REACT_APP_TEST_API_KEY}
+    </div>
+  );
+}
+
+export default App;
