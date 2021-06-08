57668f57cb49f0ad1fad4294fa445ba0da3cd902
GoodPart <pks940122@cttd.co.kr>
2021-06-08 23:30:49 +0900

    dotenv 수정, 한글 소환사 이름 인코딩 추가

--- front/server/server.js
+++ front/server/server.js
@@ -1,5 +1,6 @@
 // import dotenv from "dotenv";
 const axios = require("axios");
+const dotenv = require("dotenv").config();
 
 const express = require("express");
 const app = express();
@@ -8,18 +9,33 @@ const port = process.env.PORT || 3001;
 
 const cors = require("cors");
 
-function lolApi() {
+const riotApiKey = process.env.REACT_APP_TEST_API_KEY;
+
+const summonerId = "구월동원빈";
+
+//소환사 닉네임 서치 및 id값 찾기
+function getRiotApiSummonerInfo() {
+  const summonerIdEnCode = encodeURI(summonerId);
+
   axios
     .get(
-      "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/kovolt?api_key=RGAPI-95c1f6e8-c4f6-4c16-9acb-8940f7060b02"
+      `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerIdEnCode}?api_key=${riotApiKey}`
     )
     .then((res) => res.data)
     .then((post) => {
       lolName = post.name;
       lolId = post.id;
+      console.log(post.name);
     });
+
+  //   console.log(encodeStr);
 }
-lolApi();
+
+//소환사 닉네임 id값으로 상태 체크
+
+// 한글 소환사 닉네임 인코딩 변환 function
+
+getRiotApiSummonerInfo();
 
 app.use(cors());
 
