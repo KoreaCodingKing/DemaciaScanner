4c201288419dc91308d34c14ac8ee0c56ba0d2a4
GoodPart <pks940122@cttd.co.kr>
2021-06-15 00:18:48 +0900

    Revert "인게임 여부,  status 상태 작업 완료"
    This reverts commit 9e20d7179060d4ce98e3c09158d4f1c27627618e.

--- front/server/server.js
+++ front/server/server.js
@@ -10,10 +10,10 @@ const cors = require("cors");
 
 const riotApiKey = process.env.REACT_APP_TEST_API_KEY;
 
-const summonerId = ["kovolt", "검은머리사나이"];
+const summonerId = ["kovolt", "구월동원빈", "저렴한 핫바"];
 let encodeIdList = [];
 
-let encodeJsonList = [];
+let testList = new Array();
 
 app.use(cors());
 
@@ -29,60 +29,30 @@ const encodeFnc = async () => {
 //axios 호출 함수
 const axiosFunc = async (encodeIdList, index) => {
   encodeIdList.map((item, index) => {
-    Promise.resolve(useAxios(item)).then((getData) => {
+    const response = axios.get(
+      `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${item}?api_key=${riotApiKey}`
+    );
+    Promise.resolve(response).then((getData) => {
       var data = new Object();
       data.number = index;
       data.name = getData.data.name;
       data.id = getData.data.id;
 
-      encodeJsonList.push(data);
+      testList.push(data);
 
-      // key number의 value로 오름차순 정렬
-      encodeJsonList.sort(function (a, b) {
-        return a.number - b.number;
-      });
-
-      // 인게임여부 확인
-      Promise.resolve(useAxiosInGame(data.id))
-        .then((getState) => {
-          // console.log(getState.data.gameMode);
-          console.log(getState.data);
-          data.status = "true";
-        })
-        .catch((err) => {
-          console.log("에러입니다", err.response.status);
-          data.status = err.response.status === 404 ? "false" : false;
-        });
+      console.log(`이름${index} - ${getData.data.name}`);
     });
   });
 
   //엔드포인트 지정
-  app.use("/userinfo", (req, res) => res.json(encodeJsonList));
+  app.use("/lol3", (req, res) => res.json(testList));
 };
 
-// axios 사용
-const useAxios = (item) => {
-  return axios.get(
-    `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${item}?api_key=${riotApiKey}`
-  );
-};
-
-// 인게임 정보 axios 사용
-const useAxiosInGame = (summonerId) => {
-  return axios.get(
-    `https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${summonerId}?api_key=${riotApiKey}`
-  );
-};
-
-//데이터 받아오기
 const getUserData = () => {
   axiosFunc(encodeIdList);
 };
 
-//조회할 아이디 리스트를 받아, 인코딩
 encodeFnc();
-
-//받은 리스트를 axios요청 json리스트화
 getUserData();
 
 app.use(bodyParser.json());
--- front/src/page/ApiTest.js
+++ front/src/page/ApiTest.js
@@ -1,7 +1,7 @@
 // 테스트중인 페이지
 // CORS policy 오류 ->브라우저에서 보내서 그럼, 서버에서 보내면 됨
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Loading from "../components/Loading";
 
@@ -10,19 +10,22 @@ function ApiTest() {
   const [id, setId] = useState("");
   const [load, setLoad] = useState(false);
 
-  const getUserInfo = async () => {
-    try {
-      setLoad(true);
-      const response = await axios.get("http://localhost:3001/userinfo");
-      Promise.resolve(response).then((getData) => {
-        setName(getData.data[0].name);
-        setId(getData.data[0].id);
-      });
-    } catch (e) {
-      console.error(e);
-    }
-    setLoad(false);
-  };
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoad(true);
+        const response = await axios.get("http://localhost:3001/lol3");
+        Promise.resolve(response).then((getData) => {
+          setName(getData.data[0].name);
+          setId(getData.data[0].id);
+        });
+      } catch (e) {
+        console.error(e);
+      }
+      setLoad(false);
+    };
+    getData();
+  }, []);
 
   if (load)
     return (
@@ -35,9 +38,6 @@ function ApiTest() {
 
   return (
     <>
-      <hr />
-      <button onClick={getUserInfo}>정보 갱신</button>
-      <br />
       아이디 : {name}
       <br />
       id : {id}
