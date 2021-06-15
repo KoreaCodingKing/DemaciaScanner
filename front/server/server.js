025db9a78f869b0e46d79908c28c17f3883c1c72
GoodPart <pks940122@cttd.co.kr>
2021-06-16 01:52:53 +0900

    데이터의 중복 제거 진행중, 판단 로직 다시 확인해야함

--- front/server/server.js
+++ front/server/server.js
@@ -10,12 +10,42 @@ const cors = require("cors");
 
 const riotApiKey = process.env.REACT_APP_TEST_API_KEY;
 
-const summonerId = ["kovolt", "검은머리사나이"];
+const summonerId = [
+  "원딜 왕자 변정현",
+  "kovolt",
+  "kovolt",
+  "틀탑라간",
+  "bbtzw",
+  "쇠똥구리a",
+  "삼성 트롤 세탁기",
+  "뜨 식",
+  "느그집옆동네",
+  "이준경 123",
+  // "엘 림",
+  // "원딜김도형2345345",
+  // "또 지고 싶다",
+  // "병시우",
+  // "고기리대장",
+  // "이상적인64종지레",
+  // "UZLL",
+  // "형 쥬지가불끈해",
+  // "혜지하면트롤함",
+  // "듀오있음겜안함",
+  // "그대의부모",
+  // "Hoits",
+  // "김서폿",
+  // "수녀님1",
+  // "boohoom",
+  // "치우지못할똥",
+  // "출동이다 김서폿",
+];
 let encodeIdList = [];
 
 let encodeJsonList = [];
 
 app.use(cors());
+
+// 중복 리스트 제거기능필요
 
 //인코딩한 아이디 리스트화 encodeIdList로 리턴
 const encodeFnc = async () => {
@@ -29,36 +59,52 @@ const encodeFnc = async () => {
 //axios 호출 함수
 const axiosFunc = async (encodeIdList, index) => {
   encodeIdList.map((item, index) => {
-    Promise.resolve(useAxios(item)).then((getData) => {
-      var data = new Object();
-      data.number = index;
-      data.name = getData.data.name;
-      data.id = getData.data.id;
+    Promise.resolve(useAxios(item))
+      // 유저 정보 조회
+      .then((getData) => {
+        var data = new Object();
+        data.number = index;
+        data.name = getData.data.name;
+        data.id = getData.data.id;
 
-      encodeJsonList.push(data);
+        encodeJsonList = encodeJsonList.concat(data);
 
-      // key number의 value로 오름차순 정렬
-      encodeJsonList.sort(function (a, b) {
-        return a.number - b.number;
+        // key number의 value로 오름차순 정렬
+        // encodeJsonList.sort(function (a, b) {
+        //   return a.number - b.number;
+        // });
+
+        // 인게임여부 확인
+        Promise.resolve(useAxiosInGame(data.id))
+          .then((getState) => {
+            // console.log(getState.data.gameMode);
+            // console.log(getState.data);
+            // console.log(getData);
+            data.status = [
+              {
+                state: getState.data.gameType,
+                gameMode: getState.data.gameMode,
+              },
+            ];
+          })
+          .catch((err) => {
+            // console.log("접속하지 않았습니다", err.response.status);
+            data.status = err.response.status === 404 ? "false" : false;
+          });
+      })
+      .catch((err, item) => {
+        // console.log("없는 아이디입니다", err.response.status, item);
+        var data = new Object();
+        data.errorId = decodeURI(item);
+        encodeJsonList = encodeJsonList.concat(data);
       });
-
-      // 인게임여부 확인
-      Promise.resolve(useAxiosInGame(data.id))
-        .then((getState) => {
-          // console.log(getState.data.gameMode);
-          console.log(getState.data);
-          data.status = "true";
-        })
-        .catch((err) => {
-          console.log("에러입니다", err.response.status);
-          data.status = err.response.status === 404 ? "false" : false;
-        });
-    });
   });
 
   //엔드포인트 지정
   app.use("/userinfo", (req, res) => res.json(encodeJsonList));
 };
+
+// 정렬
 
 // axios 사용
 const useAxios = (item) => {
