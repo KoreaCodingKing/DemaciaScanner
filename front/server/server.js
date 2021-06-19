252c8579362bc695bc0591cc4f68f3c07a34382b
KoreaCodingKing <50541727+KoreaCodingKing@users.noreply.github.com>
2021-06-19 14:55:03 +0900

    todo추가, 코드 정리

--- front/server/server.js
+++ front/server/server.js
@@ -8,125 +8,91 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001;
 const cors = require("cors");
 
-const riotApiKey = process.env.REACT_APP_TEST_API_KEY;
+const riotApiKey = 'RGAPI-4da64f91-ab57-4628-b5f8-c8999af1ab91';
 
-const summonerId = [
-  "원딜 왕자 변정현",
-  "kovolt",
-  "kovolt",
-  "kovolt",
+const summonersId = [
   "kovolt",
   "kovolt1",
-  "틀탑라간",
-  "bbtzw",
-  "쇠똥구리a",
-  "삼성 트롤 세탁기",
-  "뜨 식",
-  "느그집옆동네",
-  "이준경 123",
-  // "엘 림",
-  // "원딜김도형2345345",
-  // "또 지고 싶다",
-  // "병시우",
-  // "고기리대장",
-  // "이상적인64종지레",
-  // "UZLL",
-  // "형 쥬지가불끈해",
-  // "혜지하면트롤함",
-  // "듀오있음겜안함",
-  // "그대의부모",
-  // "Hoits",
-  // "김서폿",
-  // "수녀님1",
-  // "boohoom",
-  // "치우지못할똥",
-  // "출동이다 김서폿",
+  // "bbtzw",
+  "저렴한 핫바"
+  // "쇠똥구리a",
+  // "삼성 트롤 세탁기",
 ];
-let encodeIdList = [];
-
-let encodeJsonList = [];
 
 app.use(cors());
 
-// 중복 리스트 제거기능필요
+//ToDo: end포인트 확인, 사용자 입력후 아이디값만 먼저 보여줄것인지, 정보를 더 빼와야하는지 확인 필요
+async function getUsersData() {
+  return Promise.all(summonersId.map((userId) => {
+    getUserData(encodeURI(userId)).then((userData) => {
+      if (userData.status === 404) {
+        throw new Error('no user')
+      }
 
-//인코딩한 아이디 리스트화 encodeIdList로 리턴
-const encodeFnc = async () => {
-  let sortFunc = summonerId.filter((item, idx, array) => {
-    return array.indexOf(item) === idx;
-  });
-  console.log(sortFunc);
-  sortFunc.map((user) => {
-    const encoding = encodeURI(user);
-    return (encodeIdList = encodeIdList.concat(encoding));
-    // 리턴값 형태 ['encoded','encoded']
-  });
-};
+      return {
+        index: index,
+        id: userData.id,
+        name: userData.name
+      }
+    }).catch((err) => {
+      console.log('에러', err)
+    })
+  }));
+}
+  // encodeIdList.map((item, index) => {
+  //   Promise.resolve(useAxios(item))
+  //     // 유저 정보 조회
+  //     .then((getData) => {
+  //       var data = new Object();
+  //       data.number = index;
+  //       data.name = getData.data.name;
+  //       data.id = getData.data.id;
 
-//axios 호출 함수
-const axiosFunc = async (encodeIdList, index) => {
-  encodeIdList.map((item, index) => {
-    Promise.resolve(useAxios(item))
-      // 유저 정보 조회
-      .then((getData) => {
-        var data = new Object();
-        data.number = index;
-        data.name = getData.data.name;
-        data.id = getData.data.id;
+  //       encodeJsonList = encodeJsonList.concat(data);
 
-        encodeJsonList = encodeJsonList.concat(data);
+  //       // key number의 value로 오름차순 정렬
+  //       encodeJsonList.sort(function (a, b) {
+  //         return a.number - b.number;
+  //       });
 
-        // key number의 value로 오름차순 정렬
-        encodeJsonList.sort(function (a, b) {
-          return a.number - b.number;
-        });
+  //       // 인게임여부 확인
+  //       Promise.resolve(useAxiosInGame(data.id))
+  //         .then((getState) => {
+  //           // console.log(getState.data.gameMode);
+  //           // console.log(getState.data);
+  //           // console.log(getData);
+  //           data.status = [
+  //             {
+  //               state: getState.data.gameType,
+  //               gameMode: getState.data.gameMode,
+  //             },
+  //           ];
+  //         })
+  //         .catch((err) => {
+  //           // console.log("접속하지 않았습니다", err.response.status);
+  //           if (err.response.status === 404) {
+  //             data.status = "OFF_LINE";
+  //           }
+  //           // data.status = err.response.status === 404 ? "false" : false;
+  //         });
+  //     })
+  //     .catch((err) => {
+  //       console.log(item);
+  //       var data = new Object();
+  //       data.errorId = decodeURI(item);
+  //       console.log("없는 아이디입니다", err.response.status, data.errorId);
+  //       encodeJsonList = encodeJsonList.concat(data);
+  //       // key number의 value로 오름차순 정렬
+  //       encodeJsonList.sort(function (a, b) {
+  //         return a.number - b.number;
+  //       });
+  //     });
+  // });
 
-        // 인게임여부 확인
-        Promise.resolve(useAxiosInGame(data.id))
-          .then((getState) => {
-            // console.log(getState.data.gameMode);
-            // console.log(getState.data);
-            // console.log(getData);
-            data.status = [
-              {
-                state: getState.data.gameType,
-                gameMode: getState.data.gameMode,
-              },
-            ];
-          })
-          .catch((err) => {
-            // console.log("접속하지 않았습니다", err.response.status);
-            if (err.response.status === 404) {
-              data.status = "OFF_LINE";
-            }
-            // data.status = err.response.status === 404 ? "false" : false;
-          });
-      })
-      .catch((err) => {
-        console.log(item);
-        var data = new Object();
-        data.errorId = decodeURI(item);
-        console.log("없는 아이디입니다", err.response.status, data.errorId);
-        encodeJsonList = encodeJsonList.concat(data);
-        // key number의 value로 오름차순 정렬
-        encodeJsonList.sort(function (a, b) {
-          return a.number - b.number;
-        });
-      });
-  });
+  // //엔드포인트 지정
+  // app.use("/userinfo", (req, res) => res.json(encodeJsonList));
+// };
 
-  //엔드포인트 지정
-  app.use("/userinfo", (req, res) => res.json(encodeJsonList));
-};
-
-// 정렬
-
-// axios 사용
-const useAxios = (item) => {
-  return axios.get(
-    `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${item}?api_key=${riotApiKey}`
-  );
-};
 
 // 인게임 정보 axios 사용
 const useAxiosInGame = (summonerId) => {
@@ -138,16 +104,15 @@ const useAxiosInGame = (summonerId) => {
 //중복 제거
 
 //데이터 받아오기
-const getUserData = () => {
-  axiosFunc(encodeIdList);
+async function getUserData(userId) {
+  return await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userId}?api_key=${riotApiKey}`)
 };
 
 //조회할 아이디 리스트를 받아, 인코딩
-encodeFnc();
 
 //받은 리스트를 axios요청 json리스트화
-getUserData();
 
+app.use("/userinfo", (req, res) => res.json(getUsersData()));
 app.use(bodyParser.json());
 app.use("/api", (req, res) => res.json({ username: "bryan" }));
 
