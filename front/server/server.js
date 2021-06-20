ae1b0ebd75fee865fb6b8c328de6d005392e3799
GoodPart <pks940122@cttd.co.kr>
2021-06-20 14:57:22 +0900

    프론트, 백 머지 - 성공

--- front/server/server.js
+++ front/server/server.js
@@ -9,13 +9,51 @@ const port = process.env.PORT || 3001;
 const cors = require("cors");
 
 const riotApiKey = process.env.REACT_APP_TEST_API_KEY;
-let userDataList = [];
+// let userDataList = [];
 
 const userId = "kovolt1";
 
 app.use(cors());
 
 usersData = async(req, res) => {
+  // const data = await new Promise((resolve, reject) => {
+  //   resolve(getUserData(encodeURI(userId)));
+  // }).then((result) => {
+  //   return {
+  //     id: result.data.id,
+  //     name: result.data.name
+  //   }
+  // }).catch((err) => {
+  //   console.log(err)
+  //   if (err.response.status === 404) {
+  //     return null;
+  //   }
+  // })
+
+  // return res.json(data)
+}
+
+async function getUserInformation() {
+  return Promise.all(summonersId.map((userId) => {
+    useAxiosInGame(encodeURI(userId)).then((userInfo) => {
+      console.log(userInfo)
+    })
+  })
+  )
+}
+
+// 인게임 정보 axios 사용
+async function getUserInGameData(userId) {
+  return await axios.get(
+    `https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${userId}?api_key=${riotApiKey}`
+  );
+};
+
+//post요청 - 클라이언트에서 보낸 아이디
+function getPost() {
+  app.post("/insertuser", async(req, res) => {
+  const userId = req.body.id;
+  console.log(userId)
   const data = await new Promise((resolve, reject) => {
     resolve(getUserData(encodeURI(userId)));
   }).then((result) => {
@@ -31,104 +69,10 @@ usersData = async(req, res) => {
   })
 
   return res.json(data)
-}
-// async function getUsersData() {
-//   userDataList = await Promise.all(summonersId.map((userId, index) => {
-//     return getUserData(encodeURI(userId)).then((userData) => {
-//       if (userData.status === 404) {
-//         throw new Error('no user')
-//       }
-//       console.log(userData.data)
-//       return {
-//         index: index,
-//         id: userData.id,
-//         name: userData.name
-//       }
-//     }).catch((err) => {
-//       console.log('err', err.code)
-//     })
-//   }));
-// }
-
-async function getUserInformation() {
-  return Promise.all(summonersId.map((userId) => {
-    useAxiosInGame(encodeURI(userId)).then((userInfo) => {
-      console.log(userInfo)
-    })
-  })
-  )
-}
-  
-  // encodeIdList.map((item, index) => {
-  //   Promise.resolve(useAxios(item))
-  //     // 유저 정보 조회
-  //     .then((getData) => {
-  //       var data = new Object();
-  //       data.number = index;
-  //       data.name = getData.data.name;
-  //       data.id = getData.data.id;
-
-  //       encodeJsonList = encodeJsonList.concat(data);
-
-  //       // key number의 value로 오름차순 정렬
-  //       encodeJsonList.sort(function (a, b) {
-  //         return a.number - b.number;
-  //       });
-
-  //       // 인게임여부 확인
-  //       Promise.resolve(useAxiosInGame(data.id))
-  //         .then((getState) => {
-  //           // console.log(getState.data.gameMode);
-  //           // console.log(getState.data);
-  //           // console.log(getData);
-  //           data.status = [
-  //             {
-  //               state: getState.data.gameType,
-  //               gameMode: getState.data.gameMode,
-  //             },
-  //           ];
-  //         })
-  //         .catch((err) => {
-  //           // console.log("접속하지 않았습니다", err.response.status);
-  //           if (err.response.status === 404) {
-  //             data.status = "OFF_LINE";
-  //           }
-  //           // data.status = err.response.status === 404 ? "false" : false;
-  //         });
-  //     })
-  //     .catch((err) => {
-  //       console.log(item);
-  //       var data = new Object();
-  //       data.errorId = decodeURI(item);
-  //       console.log("없는 아이디입니다", err.response.status, data.errorId);
-  //       encodeJsonList = encodeJsonList.concat(data);
-  //       // key number의 value로 오름차순 정렬
-  //       encodeJsonList.sort(function (a, b) {
-  //         return a.number - b.number;
-  //       });
-  //     });
-  // });
-
-  // //엔드포인트 지정
-  // app.use("/userinfo", (req, res) => res.json(encodeJsonList));
-// };
-
-
-// 인게임 정보 axios 사용
-async function getUserInGameData(userId) {
-  return await axios.get(
-    `https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${userId}?api_key=${riotApiKey}`
-  );
-};
-
-//post요청 - 클라이언트에서 보낸 아이디
-function getPost() {
-  app.post("/insertuser", (req, res) => {
-    const text = req.body.id;
-    summonerId = summonerId.concat(text);
-    console.log(summonerId);
-    encodeFnc(summonerId);
-    axiosFunc(encodeIdList);
+    // summonerId = summonerId.concat(text);
+    // console.log(summonerId);
+    // encodeFnc(summonerId);
+    // axiosFunc(encodeIdList);
   });
 }
 
@@ -140,11 +84,15 @@ async function getUserData(userId) {
 };
 
 //받은 리스트를 axios요청 json리스트화
-app.use("/userinfo/userlist", (req, res) => res.json(getUserInformation()))
+// app.use("/userinfo/userlist", (req, res) => res.json(getUserInformation()))
 
 app.use(bodyParser.json());
-app.get("/userinfo", usersData);
+// app.get("/userinfo", usersData);
 app.use("/api", (req, res) => res.json({ username: "bryan" }));
+
+
+// POST 받는 곳
+getPost();
 
 app.listen(port, () => {
   console.log(`express is running on ${port}`);
