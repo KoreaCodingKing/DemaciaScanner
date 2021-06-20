d2f7b99a3b473d9e12afa6c465ae9006b2b7a735
KoreaCodingKing <50541727+KoreaCodingKing@users.noreply.github.com>
2021-06-20 15:36:55 +0900

    server.js 코드 정리

--- front/server/server.js
+++ front/server/server.js
@@ -1,4 +1,3 @@
-// import dotenv from "dotenv";
 const axios = require("axios");
 const dotenv = require("dotenv").config();
 
@@ -7,40 +6,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001;
 const cors = require("cors");
-
-const riotApiKey = process.env.REACT_APP_TEST_API_KEY;
-// let userDataList = [];
-
-const userId = "kovolt1";
-
 app.use(cors());
 
-usersData = async(req, res) => {
-  // const data = await new Promise((resolve, reject) => {
-  //   resolve(getUserData(encodeURI(userId)));
-  // }).then((result) => {
-  //   return {
-  //     id: result.data.id,
-  //     name: result.data.name
-  //   }
-  // }).catch((err) => {
-  //   console.log(err)
-  //   if (err.response.status === 404) {
-  //     return null;
-  //   }
-  // })
+const riotApiKey = process.env.REACT_APP_TEST_API_KEY;
 
-  // return res.json(data)
-}
-
-async function getUserInformation() {
-  return Promise.all(summonersId.map((userId) => {
-    useAxiosInGame(encodeURI(userId)).then((userInfo) => {
-      console.log(userInfo)
-    })
-  })
-  )
-}
+async function getUserData(userId) {
+  return await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userId}?api_key=${riotApiKey}`)
+};
 
 // 인게임 정보 axios 사용
 async function getUserInGameData(userId) {
@@ -50,10 +22,8 @@ async function getUserInGameData(userId) {
 };
 
 //post요청 - 클라이언트에서 보낸 아이디
-function getPost() {
-  app.post("/insertuser", async(req, res) => {
+app.post("/insertuser", async(req, res) => {
   const userId = req.body.id;
-  console.log(userId)
   const data = await new Promise((resolve, reject) => {
     resolve(getUserData(encodeURI(userId)));
   }).then((result) => {
@@ -68,31 +38,10 @@ function getPost() {
     }
   })
 
-  return res.json(data)
-    // summonerId = summonerId.concat(text);
-    // console.log(summonerId);
-    // encodeFnc(summonerId);
-    // axiosFunc(encodeIdList);
-  });
-}
-
-//중복 제거
-
-//데이터 받아오기
-async function getUserData(userId) {
-  return await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userId}?api_key=${riotApiKey}`)
-};
-
-//받은 리스트를 axios요청 json리스트화
-// app.use("/userinfo/userlist", (req, res) => res.json(getUserInformation()))
+  return res.json(data);
+});
 
 app.use(bodyParser.json());
-// app.get("/userinfo", usersData);
-app.use("/api", (req, res) => res.json({ username: "bryan" }));
-
-
-// POST 받는 곳
-getPost();
 
 app.listen(port, () => {
   console.log(`express is running on ${port}`);
