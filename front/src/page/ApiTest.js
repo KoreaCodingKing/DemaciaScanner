23cde5228b29d1ce277f05749f6a302ac7e3649d
GoodPart <pks940122@cttd.co.kr>
2021-06-30 23:02:41 +0900

    일단 푸시

--- front/src/page/ApiTest.js
+++ front/src/page/ApiTest.js
@@ -1,18 +1,18 @@
 // 테스트중인 페이지
 // CORS policy 오류 ->브라우저에서 보내서 그럼, 서버에서 보내면 됨
 
-import React, { useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Loading from "../components/Loading";
 import UserInsertForm from "../components/UserInsertForm";
 import UserList from "../components/UserList";
 
 function ApiTest() {
-  const [status, setStatus] = useState(false)
+  const [status, setStatus] = useState(false);
   const [inputText, setInputText] = useState("");
   const [name, setName] = useState("");
   const [id, setId] = useState("");
-  const [idList, setIdList]= useState([]);
+  const [idList, setIdList] = useState([]);
   const [load, setLoad] = useState(false);
 
   const getUserInfo = async () => {
@@ -20,7 +20,7 @@ function ApiTest() {
       setLoad(true);
       const response = await axios.get("http://localhost:3001/insertuser");
       Promise.resolve(response).then((getData) => {
-        console.log(getData.data[0])
+        console.log(getData.data[0]);
         setName(getData.data[0].name);
         setId(getData.data[0].id);
       });
@@ -29,7 +29,6 @@ function ApiTest() {
     }
     setLoad(false);
   };
-
 
   // const handleSubmit = (e) => {
   //   e.preventDefault();
@@ -45,31 +44,33 @@ function ApiTest() {
     setInputText("");
   };
 
-// server.js에서 압력받은 id 값 가져오기
- const getUserData =  async (inputText) => {
+  // server.js에서 압력받은 id 값 가져오기
+  const getUserData = async (inputText) => {
     const data = new Object({
       id: inputText,
     });
-     return await axios.post("http://localhost:3001/insertuser", {
-        id: data.id,
-      })
-  }
+    return await axios.post("http://localhost:3001/insertuser", {
+      id: data.id,
+    });
+  };
 
-// 중복 아이디 값 확인 함수 return true, false
-const duplicateId = (inputText) => {
-}
+  const getUserIngameData = async (accountId) => {};
+
+  // 중복 아이디 값 확인 함수 return true, false
+  const duplicateId = (inputText) => {};
 
   const insertUser = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if(!inputText) {
-      alert("값이 없습니다")
+    localStorage.Test = "Sample";
+
+    if (!inputText) {
+      alert("값이 없습니다");
       return false;
-
-    } else if(inputText) {
-      // 중복제거입력 불가 
-      for(let i = 0; i<idList.length; i++) {
-        if(idList[i].name.toUpperCase() == inputText.toUpperCase()) {
+    } else if (inputText) {
+      // 중복제거입력 불가
+      for (let i = 0; i < idList.length; i++) {
+        if (idList[i].name.toUpperCase() == inputText.toUpperCase()) {
           onReset();
           return alert("중복중복");
           break;
@@ -77,28 +78,25 @@ const duplicateId = (inputText) => {
       }
       getUserData(inputText)
         .then((res) => {
-          if(res.data ===null) {
-            setStatus(false)
+          if (res.data === null) {
+            setStatus(false);
             return false;
           }
           setStatus(true);
 
-          const user =  {
-              name : res.data.name,
-              id : res.data.id
-            }
+          const user = {
+            name: res.data.name,
+            id: res.data.id,
+          };
 
-          setIdList(idList.concat(user))
+          setIdList(idList.concat(user));
         })
         .catch((err) => {
-          setIdList([
-            ...idList
-          ]);
+          setIdList([...idList]);
         });
       onReset();
-    };
+    }
   };
-  
 
   if (load)
     return (
@@ -115,7 +113,12 @@ const duplicateId = (inputText) => {
       <button onClick={getUserInfo}>정보 갱신</button>
       <br />
       <form className="insert_form" onSubmit={insertUser}>
-        <UserInsertForm onInsertUser={insertUser} userValue={inputText} existValue={status} onChangeEvent={onChangeHandle} />
+        <UserInsertForm
+          onInsertUser={insertUser}
+          userValue={inputText}
+          existValue={status}
+          onChangeEvent={onChangeHandle}
+        />
       </form>
       <br />
       <UserList users={idList} />
