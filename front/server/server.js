91960ad34da7b5fcab5b0e54295cec70e3140167
GoodPart <pks940122@cttd.co.kr>
2021-07-01 17:59:18 +0900

    작업중

--- front/server/server.js
+++ front/server/server.js
@@ -15,79 +15,83 @@ let globalListState = [];
 app.use(cors());
 app.use(bodyParser.json());
 
-
-
 async function getUserData(userId) {
-  return await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userId}?api_key=${riotApiKey}`)
-};
+  return await axios.get(
+    `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userId}?api_key=${riotApiKey}`
+  );
+}
 
 // 인게임 정보 axios 사용
 async function getUserInGameData(accountId) {
   return await axios.get(
     `https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${accountId}?api_key=${riotApiKey}`
   );
-};
+}
 
 //post요청 - 클라이언트에서 보낸 아이디
-app.post("/insertuser", async(req, res) => {
+app.post("/insertuser", async (req, res) => {
   const userId = req.body.id;
   const data = await new Promise((resolve, reject) => {
     resolve(getUserData(encodeURI(userId)));
   })
-  .then((result) => {
-    console.log(`id : ${result.data.id}`, `name : ${result.data.name}`)
-    globalList = globalList.concat({
-      id : result.data.id,
-      name : result.data.name
-    })
-    app.get('/insertuser', (req,res)=> {
-      res.json(globalList)
-    } )
+    .then((result) => {
+      console.log(`id : ${result.data.id}`, `name : ${result.data.name}`);
+      globalList = globalList.concat({
+        id: result.data.id,
+        name: result.data.name,
+      });
+      app.get("/insertuser", (req, res) => {
+        res.json(globalList);
+      });
 
-    return {
-      id: result.data.id,
-      name: result.data.name
-    }
-  }).catch((err) => {
-    console.log(`없는 아이디입니다.-${userId}-${err.response.status}`)
-    if (err.response.status === 404) {
-      return null;
-    }
-  })
+      return {
+        id: result.data.id,
+        name: result.data.name,
+      };
+    })
+    .catch((err) => {
+      console.log(`없는 아이디입니다.-${userId}-${err.response.status}`);
+      if (err.response.status === 404) {
+        return null;
+      }
+    });
   return res.json(data);
 });
 
-app.get("/userstate", async(req,res) => {
-  
-  globalList.map((item, index)=> {
+app.get("/userstate", async (req, res) => {
+  globalList.map((item, index) => {
     const userName = item.name;
     const userAccountId = item.id;
     const data = new Promise((resolve, reject) => {
       resolve(getUserInGameData(encodeURI(userAccountId)));
     })
-    .then((result)=> {
-      console.log(` No. ${index+1} Name : ${item.name} gameType : ${result.data.gameType}`, `gameMode : ${result.data.gameMode}`)
-      globalListState = globalListState.concat({
-        name :result.name,
-        status : true 
-      })
-      res.json(globalListState)
-    })
-    .catch((err)=> {
-      if (err.response.status === 404) {
-        data.status = "OFF_LINE";
+      .then((result) => {
+        console.log(
+          ` No. ${index + 1} Name : ${item.name} gameType : ${
+            result.data.gameType
+          }`,
+          `gameMode : ${result.data.gameMode}`
+        );
         globalListState = globalListState.concat({
-          name : userName,
-          status : false
-        })
-        res.json(globalListState)
-        // console.log(userAccountId)
-        // console.log("OFF_Line")
-      }
-    })
-  })
-
-})
+          name: result.name,
+          status: true,
+        });
+        res.json(globalListState);
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          data.status = "OFF_LINE";
+          globalListState = globalListState.concat({
+            name: userName,
+            status: false,
+          });
+          res.json(globalListState);
+          // console.log(userAccountId)
+          // console.log("OFF_Line")
+        }
+      });
+  });
+});
 
 app.listen(port, () => {
   console.log(`express is running on ${port}`);
