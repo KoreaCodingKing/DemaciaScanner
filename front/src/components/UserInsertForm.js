089ebcf722dd9197f36e75e76781eac00b25f2dc
KoreaCodingKing <50541727+KoreaCodingKing@users.noreply.github.com>
2021-07-03 20:24:16 +0900

    input tag wrong typo fix

--- front/src/components/UserInsertForm.js
+++ front/src/components/UserInsertForm.js
@@ -7,7 +7,7 @@ const UserInsertForm = (props) => {
   // console.log(props.userValue)
   return (
     <>
-      <input onChange={props.onChangeEvent} inputValue={props.userValue} />
+      <input onChange={props.onChangeEvent} value={props.inputValue} />
       <button onClick={props.onInsertUser}>접수</button>
       <span className="state">{props.existValue ? "true" : "false"}</span>
     </>
