e5db54b2440ffa0da23203f38e1716f539716a6a
yrkim <yrkim@pocl.io>
2021-07-18 12:34:02 +0900

    서버 수정사항 반영

--- front/server/server.js
+++ front/server/server.js
@@ -16,15 +16,15 @@ let count = 0;
 app.use(cors());
 app.use(bodyParser.json());
 
-async function getUserData(userId) {
+async function getUserData(userName) {
   return await axios.get(
-    `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userId}?api_key=${riotApiKey}`
+    `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${userName}?api_key=${riotApiKey}`
   );
 }
 
 // 인게임 정보 axios 사용
 async function getUserInGameData(data) {
-  const userId = data.accountId;
+  const userId = data.id;
   // console.log(data.name);
   return await axios.get(
     `https://kr.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${userId}?api_key=${riotApiKey}`
@@ -37,7 +37,6 @@ async function getTempIdList() {
     `https://kr.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/CHALLENGER/I?page=1&api_key=${riotApiKey}`
   );
 }
-
 // 테스트용 첼린저 데이터
 app.get("/testlist", async (req, res) => {
   const data = await new Promise((resolve, reject) => {
@@ -50,46 +49,62 @@ app.get("/testlist", async (req, res) => {
 
 //post요청 - 클라이언트에서 보낸 아이디
 app.post("/searchuser", async (req, res) => {
-  const userId = req.body.name;
+  const userName = req.body.name;
   const data = await new Promise((resolve, reject) => {
-    resolve(getUserData(encodeURI(userId)));
-  }).then((result) => {
-    return {
-      id: result.data.id,
-      name: result.data.name,
-    };
-  }).catch((err) => {
-    console.log(`없는 아이디입니다.-${userId}-${err.response.status}`);
-    if (err.response.status === 404) {
-      return null;
-    }
-  });
+    resolve(getUserData(encodeURI(userName)));
+  })
+    .then((result) => {
+      // globalList = globalList.concat({
+      //   id: result.data.id,
+      //   name: result.data.name,
+      // });
+      // app.get("/searchuser", (req, res) => {
+      //   res.json(globalList);
+      // });
+
+      return {
+        id: result.data.id,
+        name: result.data.name,
+        accountId: result.data.accountId
+        
+      };
+    })
+    .catch((err) => {
+      console.log(`없는 아이디입니다.-${userName}-${err.response.status}`);
+      if (err.response.status === 404) {
+        return null;
+      }
+    });
   return res.json(data);
 });
 
 app.post("/userstatus", async (req, res) => {
   const userList = req.body.users;
   let asdList = [];
-  userList.map((item, index) => {
-    ((x) => {
-      setTimeout(() => {
-        new Promise((resolve) => {
-          resolve(getUserInGameData(item));
-        }).then((res) => {
+
+  userList.map((user, index) => {
+    setTimeout(() => {
+      // console.log(user.accountId)
+      new Promise((resolve) => {
+        resolve(getUserInGameData(user));
+      }).then((res) => {
           const timeStamp = new Date(res.data.gameStartTime);
-          console.log(`${item.name} 게임중임 - ${timeStamp}`);
-          // console.log(res.data.gameStartTime);
+          console.log(`${user.name} 게임중임 - ${timeStamp}`);
+          console.log(res.data.participants);
           //           gameStartTime: 1625966487381,
           //           gameLength: 628
           asdList = asdList.concat({
-            name: item.name,
-            state: true,
+            name: user.name,
             currentTimeStamp: timeStamp,
+            state: true,
+            accountId : user.accountId,
+            participants : res.data.participants
           });
         }).catch((err) => {
-          console.log(`니가 찾는 ${item.name} 안들어왔어 ㅡㅡ`);
+          console.log(`니가 찾는 ${user.name} 안들어왔어 ㅡㅡ`);
           asdList = asdList.concat({
-            name: item.name,
+            name: user.name,
+            accountId: user.accountId,
             state: false,
           });
         }).finally(() => {
@@ -97,8 +112,7 @@ app.post("/userstatus", async (req, res) => {
             return res.json(asdList);
           }
         });
-      }, 250 * x);
-    })(index);
+    }, 500);
   });
 });
 
