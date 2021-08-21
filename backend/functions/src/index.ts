f80e85cfbfbfcab7b415a37d3669b6e4d3279c4f
yrkim <yrkim@pocl.io>
2021-08-21 16:03:37 +0900

    ip setting 완료, login, signup swtting

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,23 +1,52 @@
+import User from './models/user';
 const express = require("express");
 const cors = require("cors");
 const bodyparser = require("body-parser");
 const dotenv = require("dotenv").config();
 
-const router = require('router');
-const db = require('mongoose');
+const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 8080;
 
-app.use(cors());
+// app.use(cors());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-db.connect(process.env.MONGODB_CONNECTOR, { useNewUrlParser: true })
-    .then(() => console.log('Successfully connected to mongodb'))
-    .catch((err: any) => console.error(err)
-);
+mongoose.connect(process.env.MONGODB_CONNECTOR, { 
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true 
+})
+.then(() => console.log('Successfully connected to mongodb'))
+.catch((err: any) => { console.log('iserror?'); console.error('1', err)});
 
-app.get('/user')
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('mongoDB connection success')
+});
+
+app.get('/login', (req: any, res: any) => {
+    User.findOne({ id: 'yrkim' }, (err: any, user: any) => {
+        return res.json({ login: true })
+    });
+})
+
+app.post('/signup', (req: any, res: any) => {
+    const newUser = new User({
+        id: 'yrkim',
+        lolid: '저렴한 핫바',
+        pw: 'zxaser1325!',
+        created: new Date()
+    });
+
+    newUser.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true,
+        });
+    })
+})
+
 app.listen(port, () => {
     console.log(`Express App on port ${port}!`);
 });
-
-module.exports = app;
--- backend/functions/src/models/user.ts
+++ backend/functions/src/models/user.ts
@@ -1,8 +1,18 @@
-const mongoose = require('mongoose');
-const schema = mongoose.Schema;
+import * as mongoose from 'mongoose';
+import { Schema } from 'mongoose';
 
-const userSchema = schema({
-    id: String,
-    lolid: String,
-    pw: String
+interface User {
+    id: string,
+    lolid: string,
+    pw: string,
+    created: Date
+};
+
+const userSchema = new Schema({
+    id: {type: String, required: true, unique: true},
+    lolid: {type: String},
+    pw: {type: String, required: true},
+    created: {type: Date, required: true,}
 });
+
+export default mongoose.model<User>('User', userSchema);
